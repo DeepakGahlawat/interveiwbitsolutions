7b771e3c65435750cea311be38a8e0fca2c664e0
DeepakGahlawat <deepakgahlawat000@gmail.com>
2022-08-03 09:45:23 +0530

    findduplicateinarray

---
+++ findduplicateinarray.java
@@ -0,0 +1,19 @@
+public class Solution {
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public int repeatedNumber(final List<Integer> A) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<A.size();i++)
+        {
+            if(set.contains(A.get(i)))
+             {return A.get(i);
+             
+             }
+             else
+             {
+                 set.add(A.get(i));
+             }
+        }
+        return -1;
+    }
+}
+
